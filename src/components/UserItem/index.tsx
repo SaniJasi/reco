2f459c6965dbb1ad363c6ea2e0be3bc7005f5437
sanijasi <sanieasi@gmail.com>
2024-10-09 10:27:30 +0300

    added border for user list

--- src/components/UserItem/index.tsx
+++ src/components/UserItem/index.tsx
@@ -2,7 +2,10 @@ import { Box, Avatar } from "@mui/material";
 
 export const UserItem = ({ data }: { data?: string }) => {
   return (
-    <Box padding={"16px"}>
+    <Box
+      padding={"16px"}
+      sx={{ "&:not(:last-child)": { borderBottom: "1px solid #E8E9FF" } }}
+    >
       {data ? (
         <Box display={"flex"} alignItems={"center"} gap={"10px"}>
           <Avatar alt={data} sx={{ width: 20, height: 20 }} />
